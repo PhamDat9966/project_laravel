58d77fca431070066658dbda1a1f201d53981088
PhamDat <phamdat999666@gmail.com>
2025-12-17 19:06:58 +0700

    02 01 03 Split file

---
+++ resources/views/admin/elements/script.blade.php
@@ -0,0 +1,13 @@
+<!-- jQuery -->
+<script src="{{asset("admin/asset/jquery/dist/jquery.min.js")}}"></script>
+<!-- Bootstrap -->
+<script src="{{asset("admin/asset/bootstrap/dist/js/bootstrap.min.js")}}"></script>
+<!-- FastClick -->
+<script src="{{asset("admin/asset/fastclick/lib/fastclick.js")}}"></script>
+<!-- NProgress -->
+<script src="{{asset("admin/asset/nprogress/nprogress.js")}}"></script>
+<!-- iCheck -->
+<script src="{{asset("admin/asset/iCheck/icheck.min.js")}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{asset("admin/build/js/custom.min.js")}}"></script>
